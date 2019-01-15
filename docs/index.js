967ecf1eaa9e6acd91a56aeaeaf6ea5234c8e539
dustin ryan-roepsch <dustinryan-roepsch@dustins-mbpagain.student.iastate.edu>
2019-01-15 01:24:37 -0600

    fancy animations and updated cdn's

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,7 @@
- function load_canvas() {
-   $("#photo-selection-submit-button").prop("disabled", true);
-   $("#photo-options").show();
-   return false;
+function load_canvas() {
+    $("#photo-selection-submit-button").prop("disabled", true);
+    $("#photo-selection").slideUp("slow", function () {
+        $("#photo-options").fadeIn();
+    });
+    return false;
 }
