ae3ee5892f9b9d7d70c1b7f818f57ecc1751578f
dustin ryan-roepsch <dustinryan-roepsch@dustins-mbpagain.student.iastate.edu>
2019-01-15 10:23:15 -0600

    done

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,28 @@
 var image = null;
 var fr = null;
 var ctx = null;
+var canvas = null;
+
+function download_photo() {
+    var dataURL = canvas.toDataURL('image/png');
+    $("#downloadButton")[0].href = dataURL;
+}
 
 function settings_changed() {
     ctx.drawImage(image, 0, 0);
+    var size = $("#labelSize")[0].value;
+    var text = "Reborn Dreams By Cindy";
+    var numPixels = Math.trunc(image.width * (size / 50) / text.length);
+    ctx.font = numPixels + "px Handlee"
+    ctx.fillText(text, image.width - ctx.measureText(text).width - 10, image.height - numPixels + 10);
 }
 
 function add_canvas() {
     var el = document.createElement("canvas");
     el.width = image.width;
     el.height = image.height;
+    el.id = "renderingCanvas"
+    canvas = el;
     ctx = el.getContext("2d");
     $("#photoHolder").append(el);
     $("#labelSize").removeAttr("disabled");
